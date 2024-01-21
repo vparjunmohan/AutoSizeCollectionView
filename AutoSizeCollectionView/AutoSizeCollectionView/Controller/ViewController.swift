4df3f6e46cb442b958cc0ccd65f1abd95436e922
Arjun Mohan <vparjunmohan@gmail.com>
2024-01-21 19:19:44 +0530

    project setup

---
+++ AutoSizeCollectionView/AutoSizeCollectionView/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoSizeCollectionView
+//
+//  Created by Arjun Mohan on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
